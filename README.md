bb83b994a29a4a8839c293a4f9ce7d7e259cf3d4
zhouxin <zhouxin06@100.me>
2021-10-21 19:27:08 +0800

    ADD:无重复字符串

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ LeetCode最热一百题
 2、两数相加
     LeetCode：https://leetcode-cn.com/problems/add-two-numbers/
     答案：https://github.com/doocs/leetcode/blob/main/solution/0000-0099/0002.Add%20Two%20Numbers/README.md
+
+3、无重复字符的最长子串
+    LeetCode：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+    答案：https://github.com/doocs/leetcode/blob/main/solution/0000-0099/0003.Longest%20Substring%20Without%20Repeating%20Characters/README.md
---
+++ algorithm/src/main/java/com/algorithm/_3_无重复字符的最长子串.java
@@ -0,0 +1,38 @@
+package com.algorithm;
+
+/**
+ *  https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ *  给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ *  输入: s = "abcabcbb"
+ *  输出: 3
+ *  解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ */
+public class _3_无重复字符的最长子串 {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        // 记录字符上一次出现的位置
+        int[] last = new int[128];
+        for (int i = 0; i < 128; i++) {
+            last[i] = -1;
+        }
+        int n = s.length();
+
+        int res = 0;
+        int start = 0; // 窗口开始位置
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i);
+            System.out.println("index:"+index);
+            start = Math.max(start, last[index] + 1);
+            System.out.println("start:"+start);
+            res = Math.max(res, i - start + 1);
+
+            last[index] = i;
+        }
+        return res;
+    }
+
+}
